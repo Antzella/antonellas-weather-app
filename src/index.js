084decd7921527d44bd0c6d8840f6f6c49152a37
Antonella <antonellascaravilli@gmail.com>
2024-10-19 20:07:05 +0200

    Styled icon injected from API and created function to display forecast from API

--- src/index.js
+++ src/index.js
@@ -53,6 +53,12 @@ function handleSearchSubmit(event) {
   searchCity(searchFormInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "c2956837ba5cd37d3f08f4dft6ba2b6o";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -60,21 +66,26 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day"> 
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">☀️</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day"> 
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+           
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+         
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>17°</strong>
+                <strong>${Math.round(day.temperature.maximum)}°</strong>
               </div>
-              <div class="weather-forecast-temperature">9°</div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}°</div>
             </div>
           </div>`;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
